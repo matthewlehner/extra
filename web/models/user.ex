ad7e5fe32093eb913087ae09fd310797813252a8
Matthew Lehner <matthew@mpl.io>
2016-10-31 17:09:30 -0700

    basic user resource.

---
+++ web/models/user.ex
@@ -0,0 +1,17 @@
+defmodule Extra.User do
+  use Extra.Web, :model
+
+  schema "users" do
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [])
+    |> validate_required([])
+  end
+end
