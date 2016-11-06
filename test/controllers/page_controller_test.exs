3f5d093ff0eff8d6d2efd61e2f89b86b107286a7
Matthew Lehner <matthew@mpl.io>
2016-11-06 00:06:40 -0700

    fixes homepage test.

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Extra.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Extra automates social media marketing"
   end
 end
