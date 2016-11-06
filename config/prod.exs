7133ce802e47718b4de72cae7a134ad0779e65be
Matthew Lehner <matthew@mpl.io>
2016-11-05 21:24:30 -0700

    updates endpoint domain.

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :extra, Extra.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "tidybot.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "https", host: "extra.social", port: 443],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
