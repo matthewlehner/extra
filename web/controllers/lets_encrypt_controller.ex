2e366a84c0797e316a3238a846dbf25bea04dd66
Matthew Lehner <matthew@mpl.io>
2016-11-06 12:14:20 -0800

    trying this again.

--- web/controllers/lets_encrypt_controller.ex
+++ web/controllers/lets_encrypt_controller.ex
@@ -3,7 +3,7 @@ defmodule Extra.LetsEncrypt do
 
   @challenges %{
     "S0j7Rz2kbWbTaHR6SY6hrSTqOWKNNctVepC6XBUMz98" => "DhsZ1GNw2mYOe9pd5m-TgNHgZs14fd96qamEXxXTfvw",
-    "H2oJq-r9ZTe0C8HtmZzwaOok6X8lhNt6-Q67kmbKTbM" => "DhsZ1GNw2mYOe9pd5m-TgNHgZs14fd96qamEXxXTfvw"
+    "ca1EiWo9x-UCtE4Lh9rBWy6NiyGdoIs929cwSFAV50U" => "DhsZ1GNw2mYOe9pd5m-TgNHgZs14fd96qamEXxXTfvw"
   }
 
   def verify(conn, %{"id" => challenge} = _params) do
