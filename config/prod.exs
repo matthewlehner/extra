b413023fa6cb071e32484d8192829dea6ff57274
Matthew Lehner <matthew@mpl.io>
2016-11-06 15:25:01 -0800

    Adds hsts headers.

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ use Mix.Config
 config :extra, Extra.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "extra.social", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [ rewrite_on: [:x_forwarded_proto],
+               hsts: :true ],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
