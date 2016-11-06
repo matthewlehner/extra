9a3a27242827746f2890ce0a9e1111b0cbd46205
Matthew Lehner <matthew@mpl.io>
2016-11-06 12:10:00 -0800

    fixes key for www domain.

--- web/controllers/lets_encrypt_controller.ex
+++ web/controllers/lets_encrypt_controller.ex
@@ -3,7 +3,7 @@ defmodule Extra.LetsEncrypt do
 
   @challenges %{
     "S0j7Rz2kbWbTaHR6SY6hrSTqOWKNNctVepC6XBUMz98" => "DhsZ1GNw2mYOe9pd5m-TgNHgZs14fd96qamEXxXTfvw",
-    "KTls2I5JbcCn8Lxpv-aoeSG43YpAZTV6wbQqoaIoul8" => "DhsZ1GNw2mYOe9pd5m-TgNHgZs14fd96qamEXxXTfvw"
+    "H2oJq-r9ZTe0C8HtmZzwaOok6X8lhNt6-Q67kmbKTbM" => "DhsZ1GNw2mYOe9pd5m-TgNHgZs14fd96qamEXxXTfvw"
   }
 
   def verify(conn, %{"id" => challenge} = _params) do
