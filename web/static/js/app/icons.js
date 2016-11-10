6564e9abab6bc2970e0ff6b9c416e96e67981823
Matthew Lehner <matthew@mpl.io>
2016-11-09 18:45:12 -0800

    first pass with icons.

---
+++ web/static/js/app/icons.js
@@ -0,0 +1,9 @@
+import "../../icons/collection.svg";
+import "../../icons/add-new.svg";
+import "../../icons/twitter-badge.svg";
+import "../../icons/facebook-badge.svg";
+import "../../icons/pinterest-badge.svg";
+import "../../icons/performance.svg";
+import "../../icons/account.svg";
+import "../../icons/logout.svg";
+
--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,9 @@
 defmodule Extra.LayoutView do
   use Extra.Web, :view
+
+  def icon(name) do
+    content_tag(:svg, class: "icon") do
+      content_tag(:use, "", "xlink:href": "##{name}-icon")
+    end
+  end
 end
