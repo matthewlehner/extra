8d8358c875b926ff9a6891911c42d5eceed6a147
Matthew Lehner <matthew@mpl.io>
2016-11-21 18:56:30 -0800

    Separates landing page css from other public css.

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,4 +1,12 @@
 defmodule Extra.LayoutView do
   use Extra.Web, :view
 
+  def body_class_name(conn) do
+    "#{controller_name(conn)}-#{Phoenix.Controller.action_name(conn)}"
+  end
+
+  defp controller_name(conn) do
+    Phoenix.Controller.controller_module(conn)
+    |> Phoenix.Naming.resource_name("Controller")
+  end
 end
