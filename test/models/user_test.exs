b62a9091e24f23fe2a0fa78511258437f287dfdf
Matthew Lehner <matthew@mpl.io>
2016-11-21 16:48:23 -0800

    Adds password and email changesets

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule Extra.UserTest do
 
   alias Extra.User
 
-  @valid_attrs %{}
+  @valid_attrs %{email: "hi@theretime.com"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -11,9 +11,20 @@ defmodule Extra.UserTest do
     assert changeset.valid?
   end
 
-  @tag skip: "Pending"
   test "changeset with invalid attributes" do
     changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+
+  test "registration_changeset with valid attributes" do
+    changeset = %User{email: "hi@there.com"}
+                |> User.registration_changeset(%{password: "its a password!"})
+    assert changeset.valid?
+  end
+
+  test "registration_changeset with invalid attributes" do
+    changeset = %User{email: "hi@there.com"}
+                |> User.registration_changeset(%{password: "short"})
     refute changeset.valid?
   end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -6,6 +6,10 @@ defmodule Extra.User do
   use Extra.Web, :model
 
   schema "users" do
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
     has_many :auth_tokens, Extra.AuthToken
     has_many :user_sessions, Extra.UserSession
 
@@ -17,7 +21,24 @@ defmodule Extra.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [])
-    |> validate_required([])
+    |> cast(params, ~w(email))
+    |> validate_required([:email])
+  end
+
+  def registration_changeset(struct, params \\ %{}) do
+    struct
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_length(:password, min: 6)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 end
