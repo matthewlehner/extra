25d40ffc7de6742d1b4aeec66236a609e10b1608
Matthew Lehner <matthew@mpl.io>
2016-11-24 10:48:02 -0800

    Fixes registration controller test.

--- test/controllers/registration_controller_test.exs
+++ test/controllers/registration_controller_test.exs
@@ -6,7 +6,7 @@ defmodule Extra.RegistrationControllerTest do
 
   test "renders form for new resources", %{conn: conn} do
     conn = get conn, registration_path(conn, :new)
-    assert html_response(conn, 200) =~ "New registration"
+    assert html_response(conn, 200) =~ "Register"
   end
 
   test "creates resource and redirects when data is valid", %{conn: conn} do
@@ -18,6 +18,6 @@ defmodule Extra.RegistrationControllerTest do
   test "does not create resource and renders errors when data is invalid",
   %{conn: conn} do
     conn = post conn, registration_path(conn, :create), user: @invalid_attrs
-    assert html_response(conn, 200) =~ "New registration"
+    assert html_response(conn, 200) =~ "Register"
   end
 end
