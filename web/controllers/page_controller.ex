25c2c102fe82e1742d3da93f7188fd7210143733
Matthew Lehner <matthew@mpl.io>
2016-12-11 16:26:33 -0800

    Adds icon demo page.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,11 +1,17 @@
 defmodule Extra.PageController do
   use Extra.Web, :controller
 
+  plug :put_layout, {Extra.LayoutView, :app}
+
   def index(conn, _params) do
     render conn, "index.html", layout: {Extra.LayoutView, "public.html"}
   end
 
   def styleguide(conn, _) do
-    render conn, "styleguide.html", layout: {Extra.LayoutView, "app.html"}
+    render conn, "styleguide.html"
+  end
+
+  def icons(conn, _) do
+    render conn, "icons.html"
   end
 end
