411714aa6abc26c798e9c2ed597fc9b7c5043139
Matthew Lehner <matthew@mpl.io>
2017-01-25 10:33:08 -0800

    Updates Let's Encrypt responses for extra.ai

--- web/controllers/lets_encrypt_controller.ex
+++ web/controllers/lets_encrypt_controller.ex
@@ -4,8 +4,8 @@ defmodule Extra.LetsEncrypt do
   @challenges %{
     "S0j7Rz2kbWbTaHR6SY6hrSTqOWKNNctVepC6XBUMz98" => "DhsZ1GNw2mYOe9pd5m-TgNHgZs14fd96qamEXxXTfvw",
     "ca1EiWo9x-UCtE4Lh9rBWy6NiyGdoIs929cwSFAV50U" => "DhsZ1GNw2mYOe9pd5m-TgNHgZs14fd96qamEXxXTfvw",
-    "HiSdFFk8dBQllSJjRcjFBAQ4RGCuekLFFt88o48cDXM" => "dTMmoG0_jpfxlLvva7qv2lww1-sE-AYxthComfBdjPc",
-    "P5H0lGtzs92N1-LoiE38PrdRKXLnT_NsGdvBscocOqI" => "dTMmoG0_jpfxlLvva7qv2lww1-sE-AYxthComfBdjPc"
+    "Ht3iwk9kkedCtkTNcH2skS5Eb82NDZlYbENPHybJH-U" => "dTMmoG0_jpfxlLvva7qv2lww1-sE-AYxthComfBdjPc",
+    "BRWZrkj4-jjAWnWARI7CM0QypQy53NQk65rJ7i_3BLk" => "dTMmoG0_jpfxlLvva7qv2lww1-sE-AYxthComfBdjPc"
   }
 
   def verify(conn, %{"id" => challenge} = _params) do
