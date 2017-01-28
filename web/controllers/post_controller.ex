07cf4cb05fa461a1471e04d3a29287ea59d020a0
Matthew Lehner <matthew@mpl.io>
2017-01-27 16:41:30 -0800

    Comments out unused PostController actions.

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -7,7 +7,8 @@ defmodule Extra.PostController do
     channels = conn.assigns[:current_user].social_channels
 
     changeset =
-      PostContent.changeset(%PostContent{}, %{})
+      %PostContent{}
+      |> PostContent.changeset()
       |> PostContent.with_channel_templates(channels)
 
     render(conn, "new.html", changeset: changeset)
@@ -32,40 +33,40 @@ defmodule Extra.PostController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    post = Repo.get!(PostContent, id)
-    render(conn, "show.html", post: post)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    post = Repo.get!(PostContent, id)
-    changeset = PostContent.changeset(post)
-    render(conn, "edit.html", post: post, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Repo.get!(PostContent, id)
-    changeset = PostContent.changeset(post, post_params)
-
-    case Repo.update(changeset) do
-      {:ok, post} ->
-        conn
-        |> put_flash(:info, "PostContent updated successfully.")
-        |> redirect(to: post_path(conn, :show, post))
-      {:error, changeset} ->
-        render(conn, "edit.html", post: post, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    post = Repo.get!(PostContent, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(post)
-
-    conn
-    |> put_flash(:info, "PostContent deleted successfully.")
-    |> redirect(to: post_path(conn, :index))
-  end
+  # def show(conn, %{"id" => id}) do
+  #   post = Repo.get!(PostContent, id)
+  #   render(conn, "show.html", post: post)
+  # end
+  #
+  # def edit(conn, %{"id" => id}) do
+  #   post = Repo.get!(PostContent, id)
+  #   changeset = PostContent.changeset(post)
+  #   render(conn, "edit.html", post: post, changeset: changeset)
+  # end
+  #
+  # def update(conn, %{"id" => id, "post" => post_params}) do
+  #   post = Repo.get!(PostContent, id)
+  #   changeset = PostContent.changeset(post, post_params)
+  #
+  #   case Repo.update(changeset) do
+  #     {:ok, post} ->
+  #       conn
+  #       |> put_flash(:info, "PostContent updated successfully.")
+  #       |> redirect(to: post_path(conn, :show, post))
+  #     {:error, changeset} ->
+  #       render(conn, "edit.html", post: post, changeset: changeset)
+  #   end
+  # end
+  #
+  # def delete(conn, %{"id" => id}) do
+  #   post = Repo.get!(PostContent, id)
+  #
+  #   # Here we use delete! (with a bang) because we expect
+  #   # it to always work (and if it does not, it will raise).
+  #   Repo.delete!(post)
+  #
+  #   conn
+  #   |> put_flash(:info, "PostContent deleted successfully.")
+  #   |> redirect(to: post_path(conn, :index))
+  # end
 end
