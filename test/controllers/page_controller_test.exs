3a60c1f15cc70a4172016ea13e1c3b0d32eca14e
Matthew Lehner <matthew@mpl.io>
2017-02-14 10:48:27 -0800

    Updates title and description.

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -4,6 +4,6 @@ defmodule Extra.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~
-           "Extra is a social media automation tool that intelligently and automatically reposts content at the perfect moment each and every day."
+           "Extra is a social media automation tool that intelligently and automatically reposts your content at the perfect moment each and every day."
   end
 end
