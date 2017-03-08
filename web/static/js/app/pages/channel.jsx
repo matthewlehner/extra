958985083ef10f5bf02eb7bdc76cac90426ba528
Matthew Lehner <matthew@mpl.io>
2017-03-08 11:33:57 -0800

    Starting work on channel page.

--- web/static/js/app/pages/channel.jsx
+++ web/static/js/app/pages/channel.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-export default function Channel() {
-  return (
-    <div>Im a channel</div>
-  );
-}
+import ToggleSwitch from "components/forms/toggle-switch";
+
+const channel = window.pageContext;
+
+const ChannelPage = () => (
+  <div>
+    <header className="heading">
+      <div className="heading__body">
+        <figure className="heading__figure">
+          <img src={channel.image} alt="Profile" />
+        </figure>
+        <h1>{channel.name}</h1>
+        <p>{channel.provider} account</p>
+      </div>
+      <div className="button button_small">
+        Delete account
+      </div>
+    </header>
+
+    <section className="channel-stats">
+      <h2>Stats</h2>
+      <p>Here are some stats about the channel.</p>
+    </section>
+
+    <section className="channel-schedule">
+      <h2>Schedule</h2>
+
+      <p>
+        <ToggleSwitch checked />
+        <strong>Schedule autopilot.</strong> Great for beginners and those with
+        massive collections.
+      </p>
+    </section>
+  </div>
+);
+
+export default ChannelPage;
