e7d76eb4170bf5db554e1576770b7169426a4d6e
Matthew Lehner <matthew@mpl.io>
2017-03-30 13:34:57 -0700

    Insert schedules for channel in migration.

--- priv/repo/migrations/20170315234959_create_schedule.exs
+++ priv/repo/migrations/20170315234959_create_schedule.exs
@@ -4,11 +4,18 @@ defmodule Extra.Repo.Migrations.CreateSchedule do
   def change do
     create table(:schedules) do
       add :autopilot, :boolean, default: true, null: false
-      add :social_channel_id, references(:social_channels, on_delete: :nothing)
+      add :social_channel_id, references(:social_channels, on_delete: :nothing), null: false
 
       timestamps()
     end
-    create index(:schedules, [:social_channel_id])
+    create index(:schedules, [:social_channel_id], unique: true)
 
+    if direction() == :up do
+      flush()
+      Extra.SocialChannel
+      |> Extra.Repo.all
+      |> Enum.map(&Ecto.build_assoc(&1, :schedule))
+      |> Enum.each(&Extra.Repo.insert(&1))
+    end
   end
 end
