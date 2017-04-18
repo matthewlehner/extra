b4bd0287a35dfa9116f7cc1103b7930410435b5f
Matthew Lehner <matthew@mpl.io>
2017-04-18 16:56:32 -0700

    Scopes resolvers to user context.

--- lib/extra/schema/resolvers/channel_resolver.ex
+++ lib/extra/schema/resolvers/channel_resolver.ex
@@ -3,17 +3,31 @@ defmodule Extra.Schema.ChannelResolver do
   Channel Resolver for GraphQL Channel object type.
   Grabs channels from the db.
   """
+  import Ecto.Query, only: [where: 2]
   alias Extra.Repo
   alias Extra.SocialChannel
 
-  def all(_args, _info) do
-    {:ok, Repo.all(SocialChannel)}
+  def all(_args, %{context: %{current_user: %{id: id}}}) do
+    channels =
+      SocialChannel
+      |> where(user_id: ^id)
+      |> Repo.all
+    {:ok, channels}
   end
 
-  def find(%{id: id}, _info) do
-    case Repo.get(SocialChannel, id) do
+  def all(_args, _info), do: {:error, "Not Authorized"}
+
+  def find(%{id: id}, %{context: %{current_user: %{id: user_id}}}) do
+    channel =
+      SocialChannel
+      |> where(user_id: ^user_id)
+      |> Repo.get(id)
+
+    case channel do
       nil     -> {:error, "Channel id #{id} not found"}
       channel -> {:ok, channel}
     end
   end
+
+  def find(_, _), do: {:error, "Not Authorized"}
 end
--- lib/extra/schema/resolvers/collection_resolver.ex
+++ lib/extra/schema/resolvers/collection_resolver.ex
@@ -3,17 +3,32 @@ defmodule Extra.Schema.CollectionResolver do
   Collection Resolver for GraphQL Collection object.
   Grabs collections from the db.
   """
+  import Ecto.Query, only: [where: 2]
   alias Extra.Repo
   alias Extra.PostCollection
 
-  def all(_args, _info) do
-    {:ok, Repo.all(PostCollection)}
+  def all(_args, %{context: %{current_user: %{id: id}}}) do
+    collections =
+      PostCollection
+      |> where(user_id: ^id)
+      |> Repo.all
+
+    {:ok, collections}
   end
 
-  def find(%{id: id}, _info) do
-    case Repo.get(PostCollection, id) do
+  def all(_args, _info), do: {:error, "Not Authorized"}
+
+  def find(%{id: id}, %{context: %{current_user: %{id: user_id}}}) do
+    collection =
+      PostCollection
+      |> where(user_id: ^user_id)
+      |> Repo.get(id)
+
+    case collection do
       nil        -> {:error, "Collection id #{id} not found"}
       collection -> {:ok, collection}
     end
   end
+
+  def find(_, _), do: {:error, "Not Authorized"}
 end
