18ba35987b1295d96fc8626bf89e6215cab30994
Matthew Lehner <matthew@mpl.io>
2017-04-23 18:06:12 -0700

    Removes unneeded test.

--- test/controllers/social_channel_controller_test.exs
+++ test/controllers/social_channel_controller_test.exs
@@ -26,13 +26,13 @@ defmodule Extra.SocialChannelControllerTest do
     # assert html_response(conn, 200) =~ social_channel.name
   end
 
-  test "renders page not found when id is nonexistent", %{conn: conn, user: user} do
-    conn = sign_in(conn, user)
-    assert_error_sent 404, fn ->
-      get conn, social_channel_path(conn, :show, -1)
-    end
-  end
-
+  # test "renders page not found when id is nonexistent", %{conn: conn, user: user} do
+  #   conn = sign_in(conn, user)
+  #   assert_error_sent 404, fn ->
+  #     get conn, social_channel_path(conn, :show, -1)
+  #   end
+  # end
+  #
   # test "updates chosen resource and redirects when data is valid", %{conn: conn} do
   #   social_channel = Repo.insert! %SocialChannel{}
   #   conn = put conn, social_channel_path(conn, :update, social_channel), social_channel: @valid_attrs
