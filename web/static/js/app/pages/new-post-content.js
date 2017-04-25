56eb8cbfe23d4af1ddbc3c408ebfead5abb27142
Matthew Lehner <matthew@mpl.io>
2017-04-25 14:40:58 -0700

    Adds addPostContent mutation.

--- web/static/js/app/pages/new-post-content.js
+++ web/static/js/app/pages/new-post-content.js
@@ -1,5 +1,19 @@
-import { graphql } from "react-apollo";
+import { graphql, compose } from "react-apollo";
 import SidebarQuery from "app/queries/sidebar-query.gql";
+import addPostContent from "app/queries/add-post-content-mutation.gql";
 import NewPostContent from "components/new-post-content";
 
-export default graphql(SidebarQuery)(NewPostContent);
+export default compose(
+  graphql(SidebarQuery),
+  graphql(addPostContent, {
+    name: "addPostContent",
+    options: {
+      updateQueries: {
+        CollectionPage: (previousData, { mutationResult }) => ({
+          ...previousData,
+          posts: [...previousData.posts, mutationResult.data.addPostContent]
+        })
+      }
+    }
+  })
+)(NewPostContent);
