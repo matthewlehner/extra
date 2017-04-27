99057121e8a38494e417077fc3ae57f68c498b30
Matthew Lehner <matthew@mpl.io>
2017-04-27 11:27:24 -0700

    Fixes modal positioning.

--- web/static/js/components/modal.jsx
+++ web/static/js/components/modal.jsx
@@ -1,50 +1,62 @@
 // @flow
 
-import React from "react";
+import React, { PureComponent } from "react";
 import LinkButton from "components/ui/link-button";
 import Icon from "components/icon";
 
-type Props = {
-  title?: string,
-  children?: React.Children,
-  onDismiss: () => void
-};
+export default class Modal extends PureComponent {
+  props: {
+    title: string | null,
+    children: React.Children,
+    onDismiss: () => void
+  }
 
-const Modal = ({ children, onDismiss, title }: Props) => {
-  const handleClick = (event:MouseEvent): void => {
+  static defaultProps = {
+    children: null
+  }
+
+  componentDidMount() {
+    if (document && document.body instanceof HTMLElement) {
+      document.body.classList.add("modal-open");
+    }
+  }
+
+  componentWillUnmount() {
+    if (document && document.body instanceof HTMLElement) {
+      document.body.classList.remove("modal-open");
+    }
+  }
+
+  handleClick = (event:MouseEvent): void => {
     if (event.currentTarget === event.target) {
-      onDismiss();
+      this.props.onDismiss();
     }
   };
 
-  return (
-    /*  eslint-disable jsx-a11y/no-static-element-interactions */
-    <div
-      className="modal__overlay"
-      onClick={handleClick}
-    >
-      <div className="modal__container">
-        <header className="modal__header">
-          <LinkButton
-            className="modal__dismiss"
-            onClick={handleClick}
-          >
-            <Icon name="close" width="0.75rem" height="0.75rem" />
-          </LinkButton>
-          { title ? <h2>{ title }</h2> : null }
-        </header>
-        <div className="modal__body">
-          {children}
+  render() {
+    const { children, title } = this.props;
+
+    return (
+      /*  eslint-disable jsx-a11y/no-static-element-interactions */
+      <div
+        className="modal__overlay"
+        onClick={this.handleClick}
+      >
+        <div className="modal__container">
+          <header className="modal__header">
+            <LinkButton
+              className="modal__dismiss"
+              onClick={this.handleClick}
+            >
+              <Icon name="close" width="0.75rem" height="0.75rem" />
+            </LinkButton>
+            { title ? <h2>{ title }</h2> : null }
+          </header>
+          <div className="modal__body">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-
-Modal.defaultProps = {
-  title: null,
-  children: null
-};
-
-export default Modal;
+    );
+  }
+}
