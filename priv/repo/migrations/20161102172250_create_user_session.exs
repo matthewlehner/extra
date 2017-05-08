88b85ce8c42838157ef8f426f734a3fef01cbdc5
Matthew Lehner <matthew@mpl.io>
2017-05-08 12:26:36 -0700

    Updates migration with correct datatype.

--- priv/repo/migrations/20161102172250_create_user_session.exs
+++ priv/repo/migrations/20161102172250_create_user_session.exs
@@ -5,7 +5,7 @@ defmodule Extra.Repo.Migrations.CreateUserSession do
     create table(:user_sessions) do
       add :user_agent, :string
       add :login_ip, :string
-      add :last_activity_at, :datetime
+      add :last_activity_at, :utc_datetime
       add :last_activity_ip, :string
       add :user_id, references(:users, on_delete: :nothing)
 
