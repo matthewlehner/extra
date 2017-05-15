6939c395cff85eecd7861359ae92bb6cddc66ed3
Matthew Lehner <matthew@mpl.io>
2017-05-15 11:06:30 -0700

    Ensures QueuedPost has related timeslot and time

--- priv/repo/migrations/20170503214001_create_queued_post.exs
+++ priv/repo/migrations/20170503214001_create_queued_post.exs
@@ -3,14 +3,15 @@ defmodule Extra.Repo.Migrations.CreateQueuedPost do
 
   def change do
     create table(:queued_posts) do
-      add :scheduled_for, :utc_datetime
-      add :timeslot_id, references(:timeslots, on_delete: :delete_all)
+      add :scheduled_for, :utc_datetime, null: false
+      add :timeslot_id, references(:timeslots, on_delete: :delete_all), null: false
       add :post_template_id, references(:post_templates, on_delete: :nilify_all)
 
       timestamps()
     end
 
     create index(:queued_posts, [:timeslot_id, :scheduled_for], unique: true)
+    create index(:queued_posts, [:timeslot_id])
     create index(:queued_posts, [:post_template_id])
   end
 end
