cbbdd1cebc55fc7a8eb31fa6bdb79e42f5245c8c
Matthew Lehner <matthew@mpl.io>
2017-06-30 16:24:25 -0700

    Fixes tests.

--- web/static/js/__test__/components/collection-posts.test.jsx
+++ web/static/js/__test__/components/collection-posts.test.jsx
@@ -1,21 +1,27 @@
 // @flow
-
 import React from "react";
 import renderer from "react-test-renderer";
+import { MemoryRouter } from "react-router-dom";
 
 import CollectionPosts from "components/collection-posts";
 
 describe("PostList component", () => {
   it("renders a list of posts", () => {
     const props = {
-      posts: [{
-        id: "1",
-        body: "hey buddy.",
-        channels: []
-      }]
+      posts: [
+        {
+          id: "1",
+          body: "hey buddy.",
+          channels: []
+        }
+      ]
     };
 
-    const tree = renderer.create(<CollectionPosts {...props} />);
+    const tree = renderer.create(
+      <MemoryRouter>
+        <CollectionPosts {...props} />
+      </MemoryRouter>
+    );
 
     expect(tree).toMatchSnapshot();
   });
