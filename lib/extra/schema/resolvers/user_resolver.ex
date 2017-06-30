01349e373a5ddace4a68de44a1c1af7a0a877008
Matthew Lehner <matthew@mpl.io>
2017-06-30 09:05:16 -0700

    Adds User.update functions

--- lib/extra/schema/resolvers/user_resolver.ex
+++ lib/extra/schema/resolvers/user_resolver.ex
@@ -2,8 +2,18 @@ defmodule Extra.Schema.Resolvers.UserResolver do
   @moduledoc """
   Functions for the user type in the GraphQL schema.
   """
-
   alias Extra.User
+  import Ecto.Changeset, only: [traverse_errors: 2]
+  import Extra.ErrorHelpers, only: [translate_error: 1]
+
+  def update(params, %{context: %{current_user: user}}) do
+    case User.update(user, params) do
+      {:ok, next_user} -> {:ok, next_user}
+      {:error, changeset} ->
+        errors = traverse_errors(changeset, &translate_error/1)
+        {:error, errors}
+    end
+  end
 
   def update_password(%{password: %{current: current, new: new}},
                       %{context: %{current_user: user}}) do
