1dd7ce12fbd76f472647fd19f254b7589eb4a1ec
Matthew Lehner <matthew@mpl.io>
2017-07-15 17:36:22 -0700

    Uses assoc with exclude

--- web/models/post_manager.ex
+++ web/models/post_manager.ex
@@ -3,28 +3,24 @@ defmodule PostManager do
   Functions for archiving post content and associated records.
   """
 
-  import Ecto.Query, only: [from: 2]
+  import Ecto.Query, only: [exclude: 2]
+  import Ecto, only: [assoc: 2]
   alias Ecto.Multi
   alias Extra.PostContent
   alias Extra.Repo
 
-  @spec archive(%PostContent{}) :: Ecto.Multi.t
+  @spec archive(%PostContent{}) ::{:ok, %{
+    post_content: %PostContent{}, queued_posts: {integer, nil | [term]}
+  }}
   def archive(%PostContent{} = content) do
     Multi.new
     |> Multi.update(:post_content, PostContent.archive_changeset(content))
     |> Multi.update_all(:queued_posts,
-                        assoc_queued_posts(content),
+                        content |> assoc(:queued_posts) |> exclude(:distinct),
                         set: [post_template_id: nil])
     |> Repo.transaction()
   end
 
   @spec archive(any) :: {:error, String.t()}
   def archive(_), do: {:error, "No post content struct given."}
-
-  def assoc_queued_posts(content) do
-    from q in Extra.QueuedPost,
-      join: p in Extra.PostTemplate,
-      on: p.post_content_id == ^content.id,
-      where: q.post_template_id == p.id
-  end
 end
