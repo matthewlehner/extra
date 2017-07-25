ddfcb167d42635ddac598fa15800da07b652363f
Matthew Lehner <matthew@mpl.io>
2017-07-24 21:07:47 -0700

    Allows posts to be archived. Closes #145

--- web/static/js/components/post/collection-post.jsx
+++ web/static/js/components/post/collection-post.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { graphql } from "react-apollo";
 
 import archivePostMutation from "../../app/queries/archive-post-content-mutation.gql";
+import collectionPageQuery from "../../app/queries/collection-page.gql";
 
 import Post from "../post";
 import PostChannelsFooter from "./channels-footer";
@@ -20,6 +21,24 @@ export default graphql(archivePostMutation, {
     handleArchive: mutate
   }),
   options: ({ id }) => ({
-    variables: { id }
+    variables: { id },
+    update: (
+      proxy,
+      {
+        data: {
+          archivePostContent: { id: postId, collection: { id: collectionId } }
+        }
+      }
+    ) => {
+      const query = {
+        query: collectionPageQuery,
+        variables: { id: collectionId }
+      };
+      const data = proxy.readQuery(query);
+      data.collection.posts = data.collection.posts.filter(
+        ({ id }) => id !== postId
+      );
+      proxy.writeQuery({ ...query, data });
+    }
   })
 })(CollectionPost);
