a86b44e5b7479fc4a8885dad35ae880c66b6dec1
Matthew Lehner <matthew@mpl.io>
2017-07-25 16:28:15 -0700

    Refactors ChannelSelector to PureComponent.

--- web/static/js/lib/forms/channel-selector.jsx
+++ web/static/js/lib/forms/channel-selector.jsx
@@ -1,53 +1,61 @@
 // @flow
-
-import React from "react";
+import React, { PureComponent } from "react";
 import Icon from "../../components/icon";
 import ChannelLogo from "../../components/channel-logo";
 
-const ChannelSelector = ({
-  options,
-  name,
-  onChange,
-  value: checkboxValues
-}: {
+type Props = {
   options: Array<{ id: string, name: string, provider: string }>,
   name: string,
   onChange: (string, {}) => void,
   value: {}
-}) => {
-  const handleChange = (event, id) => {
-    const newValue = { ...checkboxValues, [id]: event.currentTarget.checked };
-    onChange(name, newValue);
+};
+
+export default class ChannelSelector extends PureComponent {
+  props: Props;
+
+  defaultProps = {
+    options: []
   };
 
-  return (
-    <div className="form__collection-wrapper">
-      {options.map(({ id: value, name: label, provider }) =>
-        <label
-          key={value}
-          htmlFor={`${name}-${value}`}
-          style={{ position: "relative" }}
-        >
-          <input
-            className="form__control"
-            type="checkbox"
-            id={`${name}-${value}`}
-            checked={checkboxValues[value] || false}
-            onChange={event => handleChange(event, value)}
-          />
-          <span className="form__indicator form__indicator_checkbox">
-            <Icon className="icon icon_checkmark" name="checkmark" />
-          </span>
-          <ChannelLogo provider={provider} />
-          {label}
-        </label>
-      )}
-    </div>
-  );
-};
+  constructor(props: Props) {
+    super(props);
 
-ChannelSelector.defaultProps = {
-  options: []
-};
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-export default ChannelSelector;
+  handleChange(event: SyntheticEvent): void {
+    const { onChange, name, value: checkboxValues } = this.props;
+    const { value, checked } = event.currentTarget;
+    const newValue = { ...checkboxValues, [value]: checked };
+    onChange(name, newValue);
+  }
+
+  render() {
+    const { options, name, value: checkboxValues } = this.props;
+    return (
+      <div className="form__collection-wrapper">
+        {options.map(({ id: value, name: label, provider }) =>
+          <label
+            key={value}
+            htmlFor={`${name}-${value}`}
+            style={{ position: "relative" }}
+          >
+            <input
+              className="form__control"
+              type="checkbox"
+              id={`${name}-${value}`}
+              value={value}
+              checked={checkboxValues[value] || false}
+              onChange={this.handleChange}
+            />
+            <span className="form__indicator form__indicator_checkbox">
+              <Icon className="icon icon_checkmark" name="checkmark" />
+            </span>
+            <ChannelLogo provider={provider} />
+            {label}
+          </label>
+        )}
+      </div>
+    );
+  }
+}
