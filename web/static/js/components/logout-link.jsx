cc3f4ef2b57d879bffc00981ee7e7efa91088223
Matthew Lehner <matthew@mpl.io>
2017-08-20 13:41:32 -0700

    Adds sidebar link hover/active state. Closes #167

--- web/static/js/components/logout-link.jsx
+++ web/static/js/components/logout-link.jsx
@@ -1,17 +1,15 @@
 // @flow
 import React from "react";
-import type { Children } from "react";
+import type { Node } from "react";
 import getCsrfToken from "../lib/csrfToken";
 
 import LinkButton from "./ui/link-button";
 
-const linkColor = "rgba(255, 255, 255, 0.8)";
-
-export default ({ children }: Children) =>
+export default ({ children }: { children: Node }) =>
   <form onSubmit={setCSRFToken} action="/logout" method="post">
     <input name="_method" type="hidden" value="delete" />
     <input name="_csrf_token" type="hidden" />
-    <LinkButton style={{ color: linkColor, fill: linkColor }}>
+    <LinkButton className="sidebar-link">
       {children}
     </LinkButton>
   </form>;
