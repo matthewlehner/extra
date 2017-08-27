11911960832a12ebb2fd13c85b508dd21fc785c4
Matthew Lehner <matthew@mpl.io>
2017-08-26 21:05:30 -0700

    Adds Extra.SocialPost schema.

---
+++ web/models/extra/social_post.ex
@@ -0,0 +1,23 @@
+defmodule Extra.Extra.SocialPost do
+  use Extra.Web, :model
+
+  schema "posts" do
+    field :content, :string
+    field :channel_post_id, :string
+    field :response, :map
+    field :published_at, Ecto.DateTime
+    belongs_to :social_channel, Extra.SocialChannel
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:content, :channel_post_id, :response, :published_at])
+    |> validate_required([:content, :channel_post_id, :response, :published_at])
+    |> unique_constraint(:channel_post_id)
+  end
+end
