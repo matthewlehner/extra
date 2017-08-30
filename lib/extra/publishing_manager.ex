dd2cbde4f3d05f42ef1af0d95773d18a037ea640
Matthew Lehner <matthew@mpl.io>
2017-08-30 13:05:17 -0700

    PublishingManager WIP

--- lib/extra/publishing_manager.ex
+++ lib/extra/publishing_manager.ex
@@ -15,17 +15,32 @@ defmodule Extra.PublishingManager do
     end
   end
 
-  defp next_queued_post(timeslot) do
+  def next_queued_post(timeslot) do
     timeslot
     |> assoc(:queued_posts)
     |> order_by(asc: :scheduled_for)
     |> limit(1)
-    |> preload([:channel, :post_template, :post_content])
+    |> join(:left, [p], c in assoc(p, :channel))
+    |> join(:left, [p, c], a in assoc(c, :authorization))
+    |> join(:left, [p], t in assoc(p, :post_template))
+    |> join(:left, [p], pc in assoc(p, :post_content))
+    |> preload([p, c, a, t, pc],
+               [channel: {c, authorization: a},
+                post_template: t,
+                post_content: pc])
   end
 
   def publish_queued_post(queued_post) do
+
+
     # After the post is confirmed to be published
     clean_up_queued_post(queued_post)
+  end
+
+  def build_post(queued_post) do
+    params = %{
+      content: queued_post.post_content.body
+    }
   end
 
   def clean_up_queued_post(queued_post) do
