f2c4ea6dd0caf7b1d1b46881441f577d7441cc85
Matthew Lehner <matthew@mpl.io>
2017-09-07 14:58:38 -0700

    Adds Absinthe instrumentation.

---
+++ lib/extra/plugs/absinthe_instrumentation_plug.ex
@@ -0,0 +1,16 @@
+defmodule Extra.AbsintheInstrumentationPlug do
+  @behaviour Plug
+  import Appsignal.Instrumentation.Helpers, only: [instrument: 3]
+
+  def init(opts), do: opts
+
+  def call(conn = %{params: %{"operationName" => operation_name}}, _) do
+    Appsignal.Transaction.set_action("Absinthe##{operation_name}")
+
+    instrument("graphql.absinthe", "Accessing GraphQL endpoint.", fn() ->
+      conn
+    end)
+  end
+
+  def call(conn, _), do: conn
+end
--- web/router.ex
+++ web/router.ex
@@ -26,7 +26,7 @@ defmodule Extra.Router do
   end
 
   pipeline :api do
-    # plug Extra.AbsinthePryInPlug
+    plug Extra.AbsintheInstrumentationPlug
     plug :fetch_session
     plug :protect_from_forgery
     plug :put_secure_browser_headers
